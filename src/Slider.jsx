ab5094c72f860d3a73966a2ed65c0234270ba6bf
rupendrachoudhary25 <rupendrachoudhary25@gmail.com>
2024-08-01 17:17:29 +0530

    Image Download

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,15 +1,13 @@
-import "./index.css";
+import Image from "./assets/pexels.webp";
 
 function Slider() {
   return (
     <>
       <div className="slider">
-        <img
-          src="https://images.pexels.com/photos/346529/pexels-photo-346529.jpeg?auto=compress&cs=tinysrgb&w=800"
-          alt=""
-        />
+        <img src={Image} alt="Slider Image" />
       </div>
     </>
   );
 }
+
 export default Slider;
